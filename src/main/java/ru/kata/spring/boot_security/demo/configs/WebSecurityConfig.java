c2716145629053997894eea54a22920726464738
Egor Pushka <pushkartegor@gmail.com>
2023-09-27 17:56:31 +0300

    add : datasource

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
     }
 
-    // аутентификация inMemory
+//     аутентификация inMemory
     @Bean
     @Override
     public UserDetailsService userDetailsService() {
